2dd1b1cd9c729181ef38bd3193198e0de87fed9f
Johann <johannmoreno08@gmail.com>
2020-01-07 18:38:05 -0500

    first commit

---
+++ js/renderizar.js
@@ -0,0 +1,7 @@
+function mostrar(id, ocultar) {
+    document.getElementById(id).style.display = 'block';
+	document.getElementById(ocultar).style.display='none';
+}
+function mensaje(id) {
+    document.getElementById(id).style.display = 'block';
+}
